e4892df95a8181053b080283be541fe54a40b554
work <kondusov.ru@gmail.com>
2026-03-14 11:15:19 +0300

    mediaQueryCss, type js

---
+++ lessons/js/type.js
@@ -0,0 +1,22 @@
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Guide/Grammar_and_types
+
+let a;
+let b = 2;
+a = 1;
+let c = 'это строка ';
+let c2 = '2';
+let d = true;
+let e = false;
+console.log(a, b, c);
+console.log(typeof(a));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(c2));
+console.log(a+b);
+console.log(a+c2);
+console.log(a*c2);
+console.log(c+d);
+console.log(b+d);
+console.log(b+e);
+c2 = parseInt(c2);
+console.log(typeof(c2));
